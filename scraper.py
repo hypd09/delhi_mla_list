cad352c9fd0495fc2c74e25dfc4ed97c4a77bff7
Rishi Arora <vellaking@mailinator.com>
2016-06-18 10:50:38 +0530

    restructured

--- scraper.py
+++ scraper.py
@@ -1,16 +1,3 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
-
-import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
 # # Write out to the sqlite database using scraperwiki library
 # scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
 #
@@ -23,20 +10,33 @@ import scraperwiki
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
 
+import sqlite3
+import os
 from bs4 import BeautifulSoup as Soup
 import json, requests, re
 import datetime
+from pandas.io.json import json_normalize
+
+DB_FILE = 'data.sqlite'
 
 rex = re.compile(r'\s+')
 numb = re.compile(r'[^0-9]')
 rdate = re.compile(r'[^a-z0-9]')
+const = re.compile(r'[^a-z0-9\-]')
+
+conn = sqlite3.connect(DB_FILE)
+c = conn.cursor()
+c.execute('drop table IF EXISTS data')
+c.execute('create table data (birth_date,"+children","+constituency",contact_details,"+education",email,image,images,link,"+marital_status",name,memberOf,other_names,"+place_of_birth","+profession",source,"+spouse")')
 
 def words2date(bdate):
     bdate = clean(rdate.sub(' ',bdate.lower()))
+    if len(bdate)<2:
+        return None
+    bdate = bdate.replace('febuary','february')
     month = ['january','february','march','april','may','june','july','august','september','october','november','december']
     bdate = bdate.split(' ')
-    print(bdate)
-    date = datetime.date(int(bdate[2]),int(month.index(bdate[1])),int(numb.sub('',bdate[0])))
+    date = datetime.date(int(bdate[2]),int(month.index(bdate[1])+1),int(numb.sub('',bdate[0])))
     return date.isoformat()
 
 def text2int(textnum, numwords={}):
@@ -79,10 +79,8 @@ def num(s):
 def clean(s):
     return rex.sub(' ',s).strip()
 
-members = []
 req = requests.get('http://delhiassembly.nic.in/aspfile/whos_who/VIthAssembly/listmembers_VIth_AssemblyWsW.htm')
 soup = Soup(req.text,'html.parser')
-members = []
 for row in soup.find_all('tr'):
     member = {}
     if 'Sr. No.' in row.text: #skipping the first one
@@ -91,8 +89,8 @@ for row in soup.find_all('tr'):
     cells = row.find_all('td')
     link = 'http://delhiassembly.nic.in/aspfile/whos_who/VIthAssembly/'+cells[1].find('a')['href']
     cls = []
-    for c in cells:
-        cls.append(clean(c.text))
+    for cell in cells:
+        cls.append(clean(cell.text))
     cells = cls
     
     det = requests.get(link)
@@ -105,13 +103,12 @@ for row in soup.find_all('tr'):
         #some complication because of an extra ':'
         detail = clean(text.split(':')[1]) if 'Profession'not in text else clean(text.split(':')[2])
         details.append(detail)
-    print(json.dumps(details,indent=4))
-    print(json.dumps(cells,indent=4))
+        
     bdate = clean(details[6])
     member = {
     'other_names' : [
         {
-            'name' : clean(cells[0]),
+            'name' : clean(cells[1]),
             'note' : 'Name with prefix'
          }
         ],
@@ -123,11 +120,11 @@ for row in soup.find_all('tr'):
     '+place_of_birth' : clean(details[7]),
     '+constituency' : {
         'name':clean(cells[5]).split('(')[0],
-        'identifier':clean(details[4].split(';')[1]),
+        'identifier':clean(cells[5]).split('(')[1],
         'classification':'constituency',
         '+state':'Delhi'
         },
-    'org:memberOf' : {
+    'memberOf' : {
         'id':clean(cells[2]),
         'name':clean(details[2])
         },
@@ -136,8 +133,8 @@ for row in soup.find_all('tr'):
     '+marital_status' : True if 'married' in clean(details[12]).lower() else False,
     '+spouse' : clean(details[13]),
     '+children' : {
-        '+male':num(details[15].lower()),
-        '+female':num(details[16].lower())
+        'male':num(details[15].lower()),
+        'female':num(details[16].lower())
         },
     'source' : 'delhiassembly.nic.in',
     'links' : [{'url':link,'note':'delhiassembly.nic.in'}]
@@ -162,6 +159,27 @@ for row in soup.find_all('tr'):
                 'value':clean(phone)
             })
     member['contact_details']=contact_details
-    members.append(member)
-    print(json.dumps(member,indent=4,sort_keys=True))
-scraperwiki.sqlite.save(unique_keys=['name'], data=members, table_name='data.sqlite')
+    print(json.dumps(member,sort_keys=True,indent=4))
+    data = [
+        json.dumps(member['birth_date'],sort_keys=True),
+        json.dumps(member['+children'],sort_keys=True),
+        json.dumps(member['+constituency'],sort_keys=True),
+        json.dumps(member['contact_details'],sort_keys=True),
+        json.dumps(member['+education'],sort_keys=True),
+        json.dumps(member['email'],sort_keys=True),
+        json.dumps(member['image'],sort_keys=True),
+        json.dumps(member['images'],sort_keys=True),
+        json.dumps(member['links'],sort_keys=True),
+        json.dumps(member['+marital_status'],sort_keys=True),
+        json.dumps(member['name'],sort_keys=True),
+        json.dumps(member['memberOf'],sort_keys=True),
+        json.dumps(member['other_names'],sort_keys=True),
+        json.dumps(member['+place_of_birth'],sort_keys=True),
+        json.dumps(member['+profession'],sort_keys=True),
+        json.dumps(member['source'],sort_keys=True),
+        json.dumps(member['+spouse'],sort_keys=True)
+        ]
+    print(data,c)
+    c.execute('insert into data values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',data)
+conn.commit()
+c.close()
